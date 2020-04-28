51098cae8132fa0edb8d21a2efc1faf85e6ab6ae
lnicastro <luciano.nicastro@gmail.com>
2020-04-28 18:01:24 +0200

    tle_satcount.sh updated

--- scripts/tle_satcount.sh
+++ scripts/tle_satcount.sh
@@ -4,7 +4,7 @@
 #
 # Edit to match your needs.
 #
-# LN @ INAF-OAS Jan. 2020.  Last change: 27/04/2020
+# LN @ INAF-OAS Jan. 2020.  Last change: 28/04/2020
 #--
 
 set +o noclobber
@@ -12,9 +12,11 @@ set +o noclobber
 #NOW=$(date +"%Y-%m-%d-%H%M")
 #NOW=$(date +"%Y%m%d")
 
-# Output log file
+# Output log file. First tw in /tmp the list of all current NORAD IDs in TLE dir.
 #/tmp/tle_satcount_$NOW.log
 OUTFILE=/tmp/tle_satcount.log
+OUTFILE_DEB=/tmp/tle_satcount_deb.log
+OUTFILE_NORAD=cur_norad_ids.lis
 
 # Where the TLE files are stored
 if [ $# -eq 0 ]; then
@@ -26,22 +28,44 @@ fi
 # Goto TLE dir
 cd $TLEDIR
 
-# Remove old log file (if it exists)
-rm -f $OUTFILE
+# Remove old log files (if they exist)
+rm -f $OUTFILE $OUTFILE_DEB $OUTFILE_NORAD
 
+# List of most relevant TLE files, excluding debris
+tles=( tle-new stations visual active analyst weather noaa goes resource sarsat dmc tdrss argos planet spire geo intelsat ses iridium iridium-NEXT starlink orbcomm globalstar amateur x-comm other-comm satnogs gorizont raduga molniya gps-ops galileo beidou sbas nnss musson science geodetic engineering education military radar cubesat other gpz gpz-plus )
 
-# List of most relevant TLE files, including debris
-tles=( tle-new stations visual active analyst weather noaa goes resource sarsat dmc tdrss argos planet spire geo intelsat ses iridium iridium-NEXT starlink orbcomm globalstar amateur x-comm other-comm satnogs gorizont raduga molniya  gps-ops galileo beidou sbas nnss musson science geodetic engineering education military radar cubesat other 2019-006 1999-025 iridium-33-debris cosmos-2251-debris gpz gpz-plus )
+# List of Debris TLE files
+tlesdeb=( 2019-006 1999-025 iridium-33-debris cosmos-2251-debris )
 
 for TLE_NAME in "${tles[@]}"
 do
 	TLE=$TLE_NAME".txt"
 	wc -l $TLE | awk '{print substr($2, 0, index($2, ".")-1)" "$1/3}' | tee -a $OUTFILE
 
+	sed -n 3~3p $TLE | cut -d " " -f 2 >> ${OUTFILE_NORAD}_tmp
 done
 
+# Debries names are all the same in a file
+for TLE_NAME in "${tlesdeb[@]}"
+do
+	TLE=$TLE_NAME".txt"
+	wc -l $TLE | awk '{print substr($2, 0, index($2, ".")-1)" "$1/3}' | tee -a $OUTFILE_DEB
+done
+
+# Count "unique" NORAD IDs
+sort -u < ${OUTFILE_NORAD}_tmp > $OUTFILE_NORAD
+rm ${OUTFILE_NORAD}_tmp
+
+n_sats=`cat $OUTFILE_NORAD | wc -l`
+
+# Debries IDs are all different so just sum the counts for each file
+n_debs=`awk '{sum += $2} END {print sum}' $OUTFILE_DEB`
+
+# The total
+n_tot=$((n_sats + n_debs))
+
 echo ""
-echo "$OUTFILE ready"
+echo "$OUTFILE | $OUTFILE_DEB | $OUTFILE_NORAD ready"
 
 #
 # Create a PHP array. Comment if not needed.
@@ -53,7 +77,10 @@ $my_satcount_arr = array(' > $PHPOUT
 
 awk '{print "\""$1"\"" " => "$2","}' $OUTFILE >> $PHPOUT
 
-# Should remove last ,
+# Here we add the count of (unique) satellites including / excluding debris
+echo "'@ALL_merged_nodeb' => $n_sats," >> $PHPOUT
+echo "'@ALL_merged' => $n_tot" >> $PHPOUT
+
 echo ');
 ?>' >> $PHPOUT
 
